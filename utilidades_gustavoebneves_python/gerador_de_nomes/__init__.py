d86953fccbd597b15de5ea3bd74bed7b2b0520b2
Gustavo Neves <geb.neves@gmail.com>
2023-01-19 21:51:21 -0300

    Melhorias no gerador de nomes
    Melhorias para não haver erro.

--- utilidades_gustavoebneves_python/gerador_de_nomes/__init__.py
+++ utilidades_gustavoebneves_python/gerador_de_nomes/__init__.py
@@ -1,24 +1,33 @@
-def gerador_nomes(quant_sobrenom = 3):
+def gerador_de_nomes(quant_sobrenom = 3):
     from random import choice
 
     nomes = ['Gustavo', 'Camila', 'Antônio', 'Aurora', 'Maria', 'Cecília', 'Laura', 'Liz', 'Rebeca', 'Ana', 'Mariana', 'Lúcia', 'Elisa', 'Miguel', 'Lucas', 'Pedro', 'Bruno', 'José', 'João', 'Davi', 'Guilherme', 'Arthur', 'Rafael', 'Daniel']
 
     lista_de_sobrenomes = ['Albuquerque', 'Almeida', 'Neves', 'Gorin', 'Barbosa', 'Bastos', 'Bernardes', 'Cardoso', 'Carmo', 'Carvalho', 'Castro', 'Coelho', 'Duarte', 'Silva', 'Andrade', 'Ferreira', 'Duarte', 'Dias', 'Freitas', 'Furtado', 'Gomes', 'Gonçalves', 'Lima', 'Lopes', 'Machado', 'Marques', 'Martins', 'Nunes', 'Nascimento', 'Oliveira', 'Pedrosa', 'Pereira', 'Pimentel', 'Ramos', 'Ribeiro', 'Rocha', 'Santana', 'Santos', 'Souza', 'Teixeira', 'Vieira']
 
-    primeiro_nome = choice(nomes)
-    sobrenome = []
+    if quant_sobrenom > len(lista_de_sobrenomes) or quant_sobrenom < 0:
+        print('\033[31mQuantidade de sobrenomes inválida!\033[m')
 
-    for i in range(0, quant_sobrenom):
-        sobrenome_temporário = choice(lista_de_sobrenomes)
-        while sobrenome_temporário in sobrenome:
-            sobrenome_temporário = choice(lista_de_sobrenomes)
-        sobrenome.append(sobrenome_temporário)
+    else:
+        primeiro_nome = choice(nomes)
+        if quant_sobrenom == 0:
+            sobrenome_completo = ''
 
-    sobrenome_completo = ' '.join(sobrenome)
- 
-    nome_completo = primeiro_nome + ' ' + sobrenome_completo
-    return nome_completo
+        else:
+            sobrenome = []
+
+            for i in range(0, quant_sobrenom):
+                sobrenome_temporário = choice(lista_de_sobrenomes)
+
+                while sobrenome_temporário in sobrenome:
+                    sobrenome_temporário = choice(lista_de_sobrenomes)
+                sobrenome.append(sobrenome_temporário)
+
+            sobrenome_completo = ' '.join(sobrenome)
+    
+        nome_completo = primeiro_nome + ' ' + sobrenome_completo
+        return nome_completo
 
 print()
-print(gerador_nomes(3))
+print(gerador_de_nomes()) # mudar o parâmetro pra qualquer número
 print()
